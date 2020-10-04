11142f1fe30a0fe1490d0b8d69863f88acea93ec
Sukhvsin2 <sukhvsingh2026@gmail.com>
2020-10-04 12:06:18 +0530

    "api hidden"

--- src/request.js
+++ src/request.js
@@ -1,4 +1,7 @@
-const API_KEY = process.env.REACT_APIKEY;
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+console.log("API-KEY: ",API_KEY);
 
 export default {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
